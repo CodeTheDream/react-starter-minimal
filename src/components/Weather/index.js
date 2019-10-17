66b231399fcb06bbae1336aab07a41656b4072e7
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-10-17 16:40:51 -0400

    commit

---
+++ src/components/Weather/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class Weather extends React.Component{
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Weather;
---
+++ src/containers/compile/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import weather from "../../components/Weather"
+
+class 
