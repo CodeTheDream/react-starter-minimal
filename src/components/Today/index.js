cffdc8e70e2ca480e03ea74449081eb4b19449d9
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-10-21 16:30:06 -0400

    finished for the day... got some code trying to fix it to make it work

--- src/components/Today/index.js
+++ src/components/Today/index.js
@@ -4,46 +4,55 @@ class Today extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            zipCode: '',
+            zip: '',
             date: '',
             temperature: '',
             humidity: '',
             error: '',
+            location: '',
         };
     }
 
     componentWillMount() {
         const url = this.getWeather()
-        const weather = this.getWeather()
     }
 
-    handleChange = (event) => {
-            this.setState({ zipCode: event.target.zipCode });
+    handleUserInput = event => {
+        let userInput = event.target.value;
+        this.setState({ userZip: userInput });
     };
+
 
     getWeather = () => {
         const apikey = process.env.REACT_APP_ID;
-        const zipcode = 27077
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + zipcode + '&APPID=' + apikey
+        let zip = this.state.userZip
+        const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + zip + '&APPID=' + apikey
         fetch(url)
         .then(response => response.json())
         .then(responseData => {
           console.log("data", responseData);
-          const weatherdata = responseData.records;
-          this.setState({ weatherdata }, () => {});
+          this.setState({ location:responseData.city }, () => {});
         });
+    }
+
+    submit = () => {
+        this.getWeather();
     }
     
         render() {
             return (
-                <form>
+
+                    <form>
                     <h1>Welcome to your Weather Forecast!</h1>
                     <label>
                         Zipcode:
-                    <input type="text" value={this.state.zipCode} onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleUserInput}/>
                     </label>
-                    <h1>{this.state.weather}</h1>
-                </form>
+                        <button type="submit" onClick={() => this.submit()}>Submit</button>
+
+                        <h3>Weather Report</h3> 
+                    </form>
+
             )
         }
     
